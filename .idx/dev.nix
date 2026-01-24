ecf42fe37aca815db0268626d4aff4c3ff7fa23d
slj3458 <slj3458@gmail.com>
2026-01-24 15:26:55 +0000

    Connected to Firebase projects

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,16 +1,15 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://firebase.google.com/docs/studio/customize-workspace?hl=en
-{
+{ pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.nodejs_20
+    pkgs.sudo
   ];
 
   # Sets environment variables in the workspace
